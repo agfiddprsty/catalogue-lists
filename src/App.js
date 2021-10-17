8e4eb621f548369ced945371b5ac894ec574194a
Agfidprasetyo <agfid11@gmail.com>
2021-10-17 16:27:25 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import './App.css';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+  const [all, setAll] = useState([])
+  const [loading, setLoading] = useState(true);
+  const URL = 'https://staging-am.awalmula.co.id/rest/default/V1/categories';
+
+  useEffect(()=> {
+    const getData = async () => {
+      const result = await axios.get(URL);
+      setAll(result.data);
+      setData(result.data);
+      setLoading(false)
+    }
+    getData();
+  }, []);
+
+  const _handleCatalogue = list => {
+    if(list.children_data.length > 0) {
+      setData(list);
+    } else {
+      // setData(all)
+    }
+  }
+
+  const _handleHome = () => {
+    setData(all);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app bg-dark d-flex justify-content-center align-items-center">
+      {loading ? (
+        <div><p>loading...</p></div>
+      ) : (
+        <div className="container">
+          <div className="text-light">
+            {data !== all ? (
+              <h6 onClick={_handleHome}>home</h6>
+            ) : <div />}
+          </div>
+          {data.children_data.map((item) => (
+            <div className="text-light text-center justify-content-center align-items-center list-catalogue">
+              <h3 onClick={() => _handleCatalogue(item)}>{item.name}</h3>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
